f9f68cf0878997e4aea69155eb38f90dc18f3687
Idadelveloper <56026636+Idadelveloper@users.noreply.github.com>
2020-04-08 18:18:38 +0100

    Update estimator.py
    covid-19 estimator function

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,18 @@
 def estimator(data):
-  return data
+    data = dict(data)
+    impact = {}
+    severeImpact = {}
+    impact['currentlyInfected'] = data['reportedCases'] * 10
+    severeImpact['currentlyInfected'] = data['reportedCases'] * 50
+    impact['infectionsByRequestedTime'] = impact['currentlyInfected'] * 2**(data['timeToElapse']//3)
+    severeImpact['infectionsByRequestedTime'] = severeImpact['currentlyInfected'] * 2**(data['timeToElapse']//3)
+    severeCasesByRequestedTime = round((15/100) * severeImpact['infectionsByRequestedTime'])
+    bedsByRequestedTime = round((35/100) * data['totalHospitalBeds'])
+    severeImpact['hospitalBedsByRequestedTime'] = bedsByRequestedTime - severeCasesByRequestedTime
+    severeImpact['casesForICUByRequestedTime'] = round((5/100) * severeImpact['infectionsByRequestedTime'])
+    severeImpact['casesForVentilatorsByRequestedTime'] = round((2/100) * severeImpact['infectionsByRequestedTime'])
+    severeImpact['dollarsInFlight'] = round(severeImpact['infectionsByRequestedTime'] * data['region']['avgDailyIncomePopulation'] * data['region']['avgDailyIncomeInUSD'] * data['timeToElapse'])
+    data = {'data': data, 
+            'impact': impact, 
+            'severeImpact': severeImpact}
+    return data
